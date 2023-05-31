629c151892f2523c6393762f1cc3ee93e412a816
biaxz <hajarboss@yahoo.com>
2023-05-31 07:20:05 +0700

    Update tts.js

--- tts.js
+++ tts.js
@@ -1,3 +1,5 @@
+// script.js
+
 // Function to populate voice select dropdown
 function populateVoiceSelect() {
   const voiceSelect = document.getElementById("voice-select");
@@ -6,7 +8,7 @@ function populateVoiceSelect() {
   function updateVoices() {
     const voices = tts.getVoices();
 
-    voices.forEach(function(voice, index) {
+    voices.forEach(function (voice, index) {
       const option = document.createElement("option");
       option.value = index;
       option.textContent = voice.name;
@@ -22,63 +24,83 @@ function populateVoiceSelect() {
 }
 
 // Event listener when Convert to Audio button is clicked
-document.getElementById("convert-btn").addEventListener("click", function(event) {
+document.getElementById("convert-btn").addEventListener("click", function (event) {
   event.preventDefault();
   const pdfFile = document.getElementById("pdf-file").files[0];
+  const textInput = document.getElementById("text-input").value.trim();
 
-  // Check if a file was selected
-  if (!pdfFile) {
-    alert("Please select a PDF file");
+  // Check if a PDF file or text input is provided
+  if (!pdfFile && !textInput) {
+    alert("Please select a PDF file or enter text");
     return;
   }
 
-  // Check if the selected file is a valid PDF file
-  if (pdfFile.type !== "application/pdf") {
-    alert("Please select a valid PDF file");
-    return;
-  }
+  let textToConvert = "";
 
-  const reader = new FileReader();
-  reader.onload = function() {
-    const pdfData = new Uint8Array(reader.result);
-    pdfjsLib.getDocument(pdfData)
-      .promise.then(function(pdf) {
-        pdf.getPage(1).then(function(page) {
-          page.getTextContent({ normalizeWhitespace: false })
-            .then(function(text) {
-              const pdfText = text.items.map(item => item.str).join("");
-              const voiceSelect = document.getElementById("voice-select");
-              const selectedVoiceIndex = voiceSelect.value;
-              const tts = window.speechSynthesis;
-              const utterance = new SpeechSynthesisUtterance(pdfText);
-              const voices = tts.getVoices();
-              utterance.voice = voices[selectedVoiceIndex];
-              utterance.rate = 0.4;
-              utterance.pitch = 1;
-              utterance.word = " ";
+  if (pdfFile) {
+    // Check if the selected file is a valid PDF file
+    if (pdfFile.type !== "application/pdf") {
+      alert("Please select a valid PDF file");
+      return;
+    }
 
-              // Create Play button
-              const playBtn = document.createElement("button");
-              playBtn.textContent = "Play";
-              playBtn.addEventListener("click", function() {
-                tts.speak(utterance);
-              });
+    const reader = new FileReader();
+    reader.onload = function () {
+      const pdfData = new Uint8Array(reader.result);
+      
+	  pdfjsLib.getDocument(pdfData)
+        .promise.then(function (pdf) {
+          const numPages = pdf.numPages;
+          let currentPage = 1;
 
-              // Append Play button to the form
-              const form = document.querySelector("form");
-              form.appendChild(playBtn);
+          function extractTextFromPage(page) {
+            return page.getTextContent().then(function (textContent) {
+              const pageText = textContent.items.map(function (item) {
+                return item.str;
+              }).join(" ");
+
+              textToConvert += pageText + " ";
+
+              if (currentPage < numPages) {
+                currentPage++;
+                return pdf.getPage(currentPage).then(extractTextFromPage);
+              } else {
+                convertTextToAudio(textToConvert);
+              }
             });
+          }
+
+          pdf.getPage(currentPage).then(extractTextFromPage);
+        })
+        .catch(function (error) {
+          console.log(error);
+          alert("Error converting PDF to text");
         });
-      })
-      .catch(function(error) {
-        console.log(error);
-        alert("Error converting PDF to text");
-      });
-  };
-  reader.readAsArrayBuffer(pdfFile);
+    };
+    reader.readAsArrayBuffer(pdfFile);
+  } else {
+    textToConvert = textInput;
+    convertTextToAudio(textToConvert);
+  }
 });
 
+// Function to convert text to audio
+function convertTextToAudio(text) {
+  const voiceSelect = document.getElementById("voice-select");
+  const selectedVoiceIndex = voiceSelect.value;
+  const tts = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  const voices = tts.getVoices();
+  utterance.voice = voices[selectedVoiceIndex];
+  utterance.rate = 0.4;
+  utterance.pitch = 1;
+  
+  // Speak the utterance
+  tts.speak(utterance);
+
+}
+
 // Event listener when the page has finished loading
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   populateVoiceSelect();
 });
